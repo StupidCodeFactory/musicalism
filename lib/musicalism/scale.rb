9f0499a28e8ebff25a3a60b2e2119c7e0fa7b2e8
Yann Marquet <ymarquet@gmail.com>
2012-09-25 00:03:38 +0100

    add support chords on scales. Only tested on major chords - WIP

--- lib/musicalism/scale.rb
+++ lib/musicalism/scale.rb
@@ -5,17 +5,53 @@ module Musicalism
     attr_reader :root_note
     attr_reader :interval_generator
 
+    DEGREES = [
+      :tonic,
+      :supertonic,
+      :mediant,
+      :subdominant,
+      :dominant,
+      :submediant,
+      :subtonic
+    ].freeze
+
     def initialize root_note
       @root_note = root_note.is_a?(Note) ? root_note : Note.new(root_note)
       @interval_generator = Interval.new
       @alterations = []
       calcuate_alterations
     end
-    
+
+    def tonic_chord
+      @tonic_chord ||= tertian_from :tonic
+    end
+
+    def supertonic_chord
+      @supertonic_chord ||= tertian_from :supertonic
+    end
+
+    def mediant_chord
+      @mediant_chord ||= tertian_from :mediant
+    end
+
+    def subdominante_chord
+      @subdominante_chord ||= tertian_from :subdominant
+    end
+
+    def dominante_chord
+      @dominante_chord ||= tertian_from :dominant
+    end
+
+    def submediant_chord
+      @submediant_chord ||= tertian_from :submediant
+    end
+
+    def subtonic_chord
+      @subtonic_chord ||= tertian_from :subtonic
+    end
+
     def notes
-      @notes = @scale_structure.inject [@root_note] do |scale, interval|
-        # return an array of possible notes
-        ap scale
+      @notes ||= @scale_structure.inject [@root_note] do |scale, interval|
         scale << @root_note.transpose(interval).detect do |n|
           next_pitch = scale[-1].pitch.gsub(/#|b/, '').next
           next_pitch = 'A' if next_pitch > 'G'
@@ -23,7 +59,7 @@ module Musicalism
         end
       end
     end
-    
+
     def calcuate_alterations
       scale = Musicalism::Note::NOTES.rotate 3
       fifth_circle_index = 0
@@ -37,6 +73,15 @@ module Musicalism
       1.upto(fifth_circle_index) do |variable|
         @alterations << scale.first
         scale = scale.rotate 7
+      end
+    end
+
+    private
+    
+    def tertian_from degree
+      [0, 2, 4].inject([]) do |chord_notes, i|
+        chord_notes << notes.rotate(DEGREES.index(degree)).rotate(i).first
+        chord_notes
       end
     end
   end
--- spec/scale_spec.rb
+++ spec/scale_spec.rb
@@ -2,15 +2,67 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Musicalism::Scale do
   describe "#notes" do
-    
+
   end
   it "should the proper notes in the scale" do
     Musicalism::MajorScale.new('C').notes.should == ['C', 'D', 'E', 'F', 'G', 'A', 'B'].map { |n| Musicalism::Note.new n }
   end
-  
+
   describe "#alterations" do
     it "should return an array with the altered note for the scale" do
-      Musicalism::MajorScale.new('D').alterations
+      Musicalism::MajorScale.new('D')
     end
+  end
+  
+  describe "Chords" do
+
+    describe "#tonic_chord" do
+      it "should return a well formed chord" do
+        Musicalism::MajorScale.new('D').tonic_chord.should == ['D', '#F', 'A'].map { |n| Musicalism::Note.new n }
+      end
+    end
+    
+    describe "#supertonic_chord" do
+      it "should return a well formed chord" do
+        s = Musicalism::MajorScale.new('bD')
+        s.supertonic_chord.should == ['bE', 'bG', 'bB'].map { |n| Musicalism::Note.new n }
+      end
+    end
+
+    describe "#mediant" do
+      it "should return a well formed chord" do
+        s = Musicalism::MajorScale.new('B')
+        s.mediant_chord.should == ['#D', '#F', '#A'].map { |n| Musicalism::Note.new n }
+      end
+    end
+
+    describe "#subdominante" do
+      it "should return a well formed chord" do
+        s = Musicalism::MajorScale.new('#E')
+        s.subdominante_chord.should == ['#A', '##C', '#E'].map { |n| Musicalism::Note.new n }
+      end
+    end
+
+    describe "#dominante" do
+      it "should return a well formed chord" do
+        s = Musicalism::MajorScale.new('#B')
+        s.dominante_chord.should == ['##F', '##A', '##C'].map { |n| Musicalism::Note.new n }
+      end
+    end
+
+    describe "#submediant" do
+      it "should return a well formed chord" do
+        s = Musicalism::MajorScale.new('bF')
+        s.submediant_chord.should == ['bD', 'bF', 'bA'].map { |n| Musicalism::Note.new n }
+      end
+    end
+
+    describe "#subtonic" do
+      it "should return a well formed chord" do
+        s = Musicalism::MajorScale.new('#D')
+        s.subtonic_chord.should == ['##C', '#E', '#G'].map { |n| Musicalism::Note.new n }
+      end
+    end
+
   end
 end
