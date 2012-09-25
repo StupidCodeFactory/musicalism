7c8b922577f7e2668d216ca200f4f22a64c1e557
Yann Marquet <ymarquet@gmail.com>
2012-09-26 00:59:43 +0100

    add missing file

---
+++ lib/musicalism/chord.rb
@@ -0,0 +1,24 @@
+module Musicalism
+
+  class Chord
+    attr_reader :notes
+    def initialize notes, options = {}
+      @notes = notes
+      inversion = options.delete(:inversion) || 0
+      inverse! inversion if inversion > 0
+    end
+    
+    def inverse depth
+      self.class.new @notes.rotate(depth)
+    end
+
+    def inverse! depth
+      @notes.rotate! depth
+    end
+
+    def == other_chord
+      @notes == other_chord.notes
+    end
+
+  end
+end
